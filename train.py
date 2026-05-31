822e6b76a9b3fd5d7bbf6596fd2daeb1c833f023
mrwanismail13-coder <mrwanismail13@gmail.com>
2026-05-31 12:12:24 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,9 @@
-train: ../train/images
-val: ../valid/images
-test: ../test/images
+train: train/images
+val: valid/images
+test: test/images
 
 nc: 2
-names: ['object_ball', 'white_cue_ball']
 
-roboflow:
-  workspace: ismails-workspace-2eanf
-  project: billiards-ball-tracking
-  version: 1
-  license: CC BY 4.0
-  url: https://universe.roboflow.com/ismails-workspace-2eanf/billiards-ball-tracking/dataset/1
+names:
+  0: object_ball
+  1: white_cue_ball
